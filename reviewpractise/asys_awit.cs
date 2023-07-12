ec543b8fb3842b77ade39145af91f42c835ce48e
gaurav2000 <gauravkawde@nimapinfotech.com>
2023-07-12 18:16:15 +0530

    done

---
+++ reviewpractise/asys_awit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace reviewpractise
+{
+    internal class asys_awit
+    {
+        static void Main()
+        {
+           asys_awit.Test1 ();
+            asys_awit.Test2();
+            asys_awit.Test3();
+            asys_awit.Test4();
+
+        }
+        public static void Test1()
+        {
+             Console.WriteLine("Task 1 is starting....");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task  1 is end");
+        }
+
+        public static void Test2()
+        {
+            Console.WriteLine("Task 2 is starting....");
+            Thread.Sleep(4000);
+            Console.WriteLine("Task  2 is end");
+        }
+        public static void Test3()
+        {
+            Console.WriteLine("Task 3 is starting....");
+            Thread.Sleep(5000);
+            Console.WriteLine("Task  3 is end");
+        }
+        public static void Test4()
+        {
+            Console.WriteLine("Task 4 is starting....");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task  4 is end");
+            Console.ReadLine();
+        }
+       
+    }
+   
+}
---
+++ reviewpractise/ays_await1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace reviewpractise
+{
+    internal class ays_await1
+    {
+        static void Main()
+        {
+          Test1();
+           Test2();
+           Test3();
+           Test4();
+            Console.ReadLine();
+
+        }
+        public static async void Test1()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 1 is starting....");
+                Thread.Sleep(2000);
+                Console.WriteLine("Task  1 is end");
+
+            });
+            
+        }
+
+       
+        public static async void Test2()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 2 is starting....");
+                Thread.Sleep(4000);
+                Console.WriteLine("Task  2 is end");
+
+            });
+
+        }
+        public static async void Test3()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 3 is starting....");
+                Thread.Sleep(5000);
+                Console.WriteLine("Task  3 is end");
+
+            });
+
+        }
+        public static async void Test4()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 4 is starting....");
+                Thread.Sleep(1000);
+                Console.WriteLine("Task  4 is end");
+                
+            });
+         
+        }
+
+
+    }
+}
