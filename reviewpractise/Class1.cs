6dba7d9092c1b74f01acb02457930e0a8d173e8b
gaurav2000 <gauravkawde@nimapinfotech.com>
2023-07-13 18:26:23 +0530

    list proagram

--- reviewpractise/Class1.cs
+++ reviewpractise/Class1.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace reviewpractise
 {
-    internal class Class1
+    class Class1
     {
+        static void Main(string[] args)
+        {
+            var list = new List<ABC>();
+
+            var list1 = new List<XYZ>();
+            list1.Add(new XYZ { Name = "1" });
+            list1.Add(new XYZ { Name = "2" });
+            list1.Add(new XYZ { Name = "3" });
+
+            list.Add(new ABC { Id = 1, xyzList = list1 });
+
+            //foreach (var item in list1)
+            //{
+            //    item1 = item.Name = item.Name.Contains("3") ? "5" : item.Name;
+            //}
+
+            //Console.WriteLine(item1);
+
+            //  list1.Contains(new XYZ { Name = "3" }) ? "5"
+            //  
+            //list1[2] = new XYZ { Name = "5" };
+            //Console.WriteLine(list1[2]);
+        
+            foreach (var item in list1) 
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine("===================");
+
+            list1[2].Name = "5";
+            foreach (var itemm in list1)
+            {
+                Console.WriteLine(itemm.Name);
+            }
+
+            Console.ReadLine();
+        }
     }
+
+    public class XYZ
+    {
+        public string Name { get; set; }
+    }
+
+    public class ABC
+    {
+        public int Id { get; set; }
+        public List<XYZ> xyzList { get; set; }
+    }
+
+
 }
--- reviewpractise/list.cs
+++ reviewpractise/list.cs
@@ -8,5 +8,48 @@ namespace reviewpractise
 {
     internal class list
     {
+        //pass of object in list
+
+        static void Main()
+        {
+            List<player> players = new List<player>();
+
+           player player=new player("Gaurav");
+            player player1 = new player("Ramesh");
+            player player2 = new player("Rajesh");
+
+            players.Add(player);
+            players.Add(player1);
+                players.Add(player2);
+            foreach (var item in players)
+            {
+                Console.WriteLine(item.username);
+            }
+            Console.WriteLine("------------------------------");
+            players[2].username="umesh";
+            players[1].username = "Mahesh";
+
+            foreach (var item in players)
+            {
+                Console.WriteLine(item.username);
+            }
+            Console.WriteLine("-----------------------------------------");
+            var items = players.OrderBy(x => x.username);
+            foreach(var name in items)
+            {
+                Console.WriteLine(name.username);
+            }
+
+            Console.ReadLine();
+        }
+    }
+    class player
+    {
+        public string username;
+
+        public player(string username)
+        {
+            this.username= username;                                 
+        }
     }
 }
