a05f9624d294196903e25327f81d56e77498b551
gaurav2000 <gauravkawde@nimapinfotech.com>
2023-07-13 11:40:06 +0530

    lamda prb

---
+++ reviewpractise/lamda1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reviewpractise
+{
+    internal class lamda1
+    {
+        static void Main()
+        {
+            List<int> number=new List<int>() { 12,13,14,15,16,17};
+            Console.WriteLine(" The List");
+            //the list
+            foreach(int num in number)
+            {
+                Console.Write(" "+num);
+               
+            }
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------");
+            //the squre by using lamda
+            var square = number.Select(x=>x*x);
+            Console.WriteLine(" The square");
+            foreach (int value in square)
+            {
+                Console.Write(" "+value);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------");
+            //by using lamda find the divisible
+
+            var div = number.FindAll(x=>(x%3)==0);
+            Console.WriteLine(" the divisible number by 3");
+            foreach (var divisible in div)
+            {
+                Console.Write(" "+divisible);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ reviewpractise/lamda2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reviewpractise
+{
+    internal class lamda2
+    {
+        //order of name from list
+        static void Main()
+        {
+            List<students> std = new List<students>()
+            {
+                new students{ id= 1,name= "Gaurav" },
+                new students{ id= 4,name= "Ramesh" },
+                new students{ id= 3,name= "Mahesh" },
+                new students{ id= 2,name= "Rajesh" }
+            };
+            foreach (students student in std) 
+            {
+            Console.WriteLine("ID : {0}, Name : {1} ",student.id,student.name);
+            }
+            Console.WriteLine("-----------------------------------------------");
+            var arranged = std.OrderBy(x=>x.id);
+
+            foreach (students stdd in arranged)
+            {
+                Console.WriteLine("ID : {0}, Name : {1} ", stdd.id, stdd.name);
+            }
+            Console.ReadLine();
+        }
+
+    }
+    class students
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+    }
+}
