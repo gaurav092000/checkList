07afee06b6e91f81ba172c0a6b46ea3aba1b5d5f
gaurav2000 <gauravkawde@nimapinfotech.com>
2023-07-14 19:02:23 +0530

    done

---
+++ reviewpractise/Class5.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reviewpractise
+{
+    class Bank
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string balance{ get; set; }
+    
+
+
+    }
+   class emp:Bank
+    { 
+        public void show(int id)
+        {
+            Console.WriteLine(id);
+        }
+        public void show(string name)
+        {
+            Console.WriteLine(name);
+        }
+        public void show(double balance)
+        {
+            Console.WriteLine(balance);
+        }
+
+    }
+    internal class Class5
+    {
+
+        static void Main()
+
+        {
+            emp e=new emp();
+            int n;
+            string name;
+            Console.WriteLine("Enter the id ");
+            n=int.Parse(Console.ReadLine());
+            e.show(n);
+            Console.WriteLine("Enter the Name of Account ");
+            name=Console.ReadLine();
+            e.show(name);
+
+            Console.ReadLine();
+        }
+
+    }
+}
