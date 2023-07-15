befee72da18210913edd2332ed3e3877da9d6a8e
gaurav2000 <gauravkawde@nimapinfotech.com>
2023-07-15 18:09:34 +0530

    staatic and instance

---
+++ reviewpractise/Class6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reviewpractise
+{
+    internal class Class6
+    {
+
+        int id ;
+        string name;
+        int age ;
+        int standard;
+
+        public  void set(int id,string name,int age, int standard )
+        {
+            this.id = id;
+            this.name = name;   
+            this.age = age;
+                this.standard = standard;
+        }
+        public void get()
+        {
+            Console.WriteLine("Id : {0}",id );
+            Console.WriteLine("Name :{0}", name);
+            Console.WriteLine("Age : {0}", age);
+            Console.WriteLine("standard : {0}", standard);
+        }
+        public static void Main()
+        {
+            Class6 c=new Class6();
+            c.set(1,"Gaurav",22,1);
+            c.get();
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ reviewpractise/instans1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reviewpractise
+{
+    class StudentS
+    {
+        int id;
+        public string FirstName;
+       public  string LastName;
+        public void printfull()
+        {
+              string fullName= this.FirstName+" "+this.LastName;
+            Console.WriteLine("The Name is :{0}",fullName);
+        }
+    }
+
+
+    internal class instans1
+    {
+        static void Main()
+        {
+            StudentS student = new StudentS();
+            student.FirstName = "Gaurav";
+            student.LastName = "Kawde ";
+            student.printfull();
+            Console.ReadLine();
+
+        }
+    }
+}
